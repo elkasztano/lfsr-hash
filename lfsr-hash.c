8212f77506489d6ac8e493620574a0cc59500ab0
elkasztano <112324044+elkasztano@users.noreply.github.com>
2022-09-04 14:25:14 +0200

    Update lfsr-hash.c
    replaced the single 128bit fibonacci lfsr with an alternating step generator based on three galois lfsrs
    fixed the generation of the initial states using stdin

--- lfsr-hash.c
+++ lfsr-hash.c
@@ -1,4 +1,5 @@
-// experimental hash generator based on 128 bit lfsr
+// experimental hash generator based on an alternating step generator (ASG)
+// The ASG was patented by C.G. Günther in 1989. According to patents.google.com the patent has already expired.
 // input from stdin is used as seed
 // for demonstration purposes only
 // do not use for cryptographic applications
@@ -10,14 +11,28 @@
 
 // declare array for 128 bit lfsr state
 static uint64_t state128[2];
+static uint64_t state127[2];
+static uint64_t state126[2];
 
-// 128 bit lfsr with taps at 128, 127, 126, 121
+// 128 bit galois lfsr with taps at 128, 127, 126, 121
 int lfsr128(void)
 {
-	uint64_t bit;
-	bit = ((state128[0] >> 0) ^ (state128[0] >> 1) ^ (state128[0] >> 2) ^ (state128[0] >> 7)) & 1ULL;
-	state128[0] = (state128[0] >> 1) | (state128[1] << 63);
-	state128[1] = (state128[1] >> 1) | (bit << 63);
+	state128[0] = state128[0] >> 1 | state128[1] << 63;
+	state128[1] = (-(state128[0] & 1ULL) & 0xe100000000000000ULL) ^ (state128[1] >> 1);
+}
+
+// 127 bit galois lfsr with taps at 127, 126, 124, 120
+int lfsr127(void)
+{
+	state127[0] = (state127[0] >> 1) | (state127[1] << 63);
+	state127[1] = (-(state127[0] & 1ULL) & 0x6880000000000000ULL) ^ (state127[1] >> 1);
+}
+
+// 126 bit galois lfsr with taps at 126, 124, 122, 119
+int lfsr126(void)
+{
+	state126[0] = (state126[0] >> 1) | (state126[1] << 63);
+	state126[1] = (-(state126[0] & 1ULL) & 0x2a40000000000000ULL) ^ (state126[1] >> 1);
 }
 
 int main(int argc, char *argv[])
@@ -33,7 +48,7 @@ int main(int argc, char *argv[])
 		switch(opt)
 		{
 			case 'h':
-				printf("usage: lfsr_hash [options]\n options:\n\t-h\tshow help and quit\n\t-n output length in bytes (must be a positive integer)\n\t-c\toutput color pattern\n");
+				printf("usage: asg_hash [options]\n options:\n\t-h\tshow help and quit\n\t-n output length in bytes (must be a positive integer)\n\t-c\toutput color pattern\n");
 				return 0;
 			case 'n':
 				n = atoi(optarg);				
@@ -50,19 +65,27 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	// define lfsr start state, can be anything except zero
+	// define lfsr start states, can be anything except zero
 	state128[0] = 0x7fe1105ab10c5e4fULL;
 	state128[1] = 0x61a27f1a2009503fULL;
+
+	state127[0] = 0x1C93370CADADC36AULL;
+	state127[1] = 0x1C90070CADADC200ULL;
+
+	state126[0] = 0x1C7557296E0727FFULL;
+	state126[1] = 0x159AABE3C6C72865ULL;
 
 	int d;
 	char c, temp;
 
-	// perform XOR-operation repeatedly on the above defined start state using bytes from stdin
+	// perform XOR-operation repeatedly on the above defined start states using bytes from stdin
 	while ((d = fgetc(stdin)) != EOF)
 	{
-		if (!(count%16)) count = 0;
+		if (!(count%48)) count = 0;
 		c = (char)d;
-		state128[count/8] ^= (uint64_t)c << count%8;
+		if (count/16 == 0) state128[(count/8)%2] ^= ((uint64_t)c << ((count%8)*8));
+		if (count/16 == 1) state127[(count/8)%2] ^= ((uint64_t)c << ((count%8)*8));
+		if (count/16 == 2) state126[(count/8)%2] ^= ((uint64_t)c << ((count%8)*8));
 		++count;
 	}
 
@@ -70,26 +93,52 @@ int main(int argc, char *argv[])
 	// a state of zero is not allowed in the lfsr, because then the output will consist of zeroes only
 	if (!(state128[0]+state128[1]))
 	{
-		printf("Start state zero!\n");
+		printf("Start state 128 zero!\n");
+		return 1;
+	}	
+	
+	if (!(state127[0]+state127[1]))
+	{
+		printf("Start state 127 zero!\n");
+		return 1;
+	}
+	
+	if (!(state126[0]+state126[1]))
+	{
+		printf("Start state 126 zero!\n");
 		return 1;
 	}
 
-	// let the lfsr run a few times before actually using it
-	// the lfsr should run least 128 times, because otherwise we just get bits from the initial state
+	// clear the most significant bit of the 127 bit lfsr state, and the 2 most significant bits of the 126 bit lfsr state
+	state127[1] &= 0x7FFFFFFFFFFFFFFFULL;
+	state126[1] &= 0x3FFFFFFFFFFFFFFFULL;
+
+	// let the lfsrs run a few times before actually using them
 	for (int i = 1; i <= 3333; ++i) lfsr128();
+	for (int i = 1; i <= 2667; ++i) lfsr127();
+	for (int i = 1; i <= 3451; ++i) lfsr126();
 	
 	// allocate memory for output
 	unsigned char *hash;
 
 	hash = (unsigned char*)calloc(n, sizeof(unsigned char));
-
-	// fill the output array with bits from the lfsr
+	
+	// fill the output array with bits from the lfsrs
 	for (int i = 0; i < n; ++i)
 	{
 		for (int j = 0; j < 8; ++j)
 		{
 			lfsr128();
-			hash[i] |= (state128[0] & 1ULL) << j;
+			if (state128[0]&1ULL)
+			{	
+				lfsr127();
+				hash[i] |= (state127[0] & 1ULL) << j;
+			}
+			else 
+			{
+				lfsr126();
+				hash[i] |= (state126[0] & 1ULL) << j;
+			}
 		}
 	}
 
